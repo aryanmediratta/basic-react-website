cda8f2b4ccb6c796d4f4cd49aef4e6a6b7ee9093
Aryan Mediratta <aryanmy2k@gmail.com>
2019-01-19 17:06:15 +0530

    seventh commit

--- Desktop/WebDev/storewebsite/src/App.js
+++ Desktop/WebDev/storewebsite/src/App.js
@@ -13,14 +13,15 @@ import 'materialize-css/dist/css/materialize.min.css';
 import { BrowserRouter as Router , Route } from 'react-router-dom';
 
 class App extends Component {
+
   render() {
     return (
      <Router>
      <div>
-     <Route exact path = '/menu' component = {Menu} />
-     <Route exact path = '/contact' component = {Contact}  />
-     <Route exact path = '/' component = {Home} />
-     <Route exact path = '/about' component = {About} />
+     <Route path = '/' component = {Home} />
+     <Route path = '/menu' component = {Menu} />
+     <Route path = '/contact' component = {Contact}  />
+     <Route path = '/about' component = {About} />
      </div>
      </Router>
    );
