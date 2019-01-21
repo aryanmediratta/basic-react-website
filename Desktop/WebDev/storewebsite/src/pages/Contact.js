86d63a16a7c4770bef8c5eee133ca94a3fedbeac
Aryan Mediratta <aryanmy2k@gmail.com>
2019-01-21 23:55:55 +0530

    added containers

--- Desktop/WebDev/storewebsite/src/pages/Contact.js
+++ Desktop/WebDev/storewebsite/src/pages/Contact.js
@@ -22,9 +22,9 @@ class Contact extends Component {
 			<div>
 			<Header/>
 			<h2 style = {h2s}> Contact Us  </h2>
-			
+			<div className = "container">
 			<p style = {h1s}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			
+			</div>
 			<div class = "row">
 			<div class="col s12 m6 l3" style={{width: '100%', height: '400px'}}>
 			<Map />
