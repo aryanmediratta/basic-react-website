45796b6b1b59ba677815c06b3c9425e4bf6322f1
Aryan Mediratta <aryanmy2k@gmail.com>
2019-01-29 20:17:24 +0530

    added logo to header

--- Desktop/WebDev/storewebsite/src/components/Header.js
+++ Desktop/WebDev/storewebsite/src/components/Header.js
@@ -1,4 +1,5 @@
 import React, {Component, Fragment} from 'react';
+import logo1 from '../images/logo6.jpg';
 
 import M from 'materialize-css/dist/js/materialize.min.js';
 
@@ -24,8 +25,10 @@ document.addEventListener('DOMContentLoaded', function() {
 			<Fragment>
 			 <nav>
     <div className="nav-wrapper blue-grey darken-3" >
-      <a href="/" className="brand-logo cyan-text text-lighten-4" >Delhi Grill</a>
-      <a href="#" data-target="mobile-demo" className="sidenav-trigger cyan-text text-lighten-4" ><i className="material-icons">menu</i></a>
+      <a href="/" className="brand-logo cyan-text text-lighten-4">
+      <img src = {logo1} class="responsive-img"/>
+      </a>
+      <a href="#" data-target="mobile-demo" className="right sidenav-trigger cyan-text text-lighten-4" ><i className="material-icons">menu</i></a>
       <ul className="right hide-on-med-and-down" >
         <li><a href="/menu" className="cyan-text text-lighten-4">Menu</a></li>
         <li><a href="/contact" className="cyan-text text-lighten-4">Contact Us</a></li>
@@ -33,10 +36,10 @@ document.addEventListener('DOMContentLoaded', function() {
     </div>
   </nav>
 
-  <ul className="sidenav" id="mobile-demo">
-    <li><a href="/">Home</a></li>
-        <li><a href="/menu">Menu</a></li>
-        <li><a href="/contact">Contact Us</a></li>
+  <ul className = "sidenav" id="mobile-demo">
+    <li><a href = "/">Home</a></li>
+        <li><a href = "/menu">Menu</a></li>
+        <li><a href = "/contact">Contact Us</a></li>
   </ul>
 			</Fragment>
 
